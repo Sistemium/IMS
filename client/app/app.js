2b475091e9d3485432b08b7b23b47f7561d3bdfc
Albert <kovalevskij.albert@gmail.com>
2016-01-09 11:21:47 +0200

    Initial commit

---
+++ client/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('imsApp', [
+  'imsApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });

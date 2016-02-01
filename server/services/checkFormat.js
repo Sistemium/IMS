a8b89a631f749e96ad49cf9c69e498e09ad801ae
Albert <kovalevskij.albert@gmail.com>
2016-02-01 11:35:55 +0200

    check format

--- server/services/checkFormat.js
+++ server/services/checkFormat.js
@@ -3,6 +3,7 @@
 const imsConfig = require('../config/environment').ims;
 const gm = require('gm').subClass({imageMagick: true});
 const winston = require('winston');
+const debug = require('debug')('ims:*');
 
 export default function (file) {
   return new Promise(function (resolve, reject) {
@@ -16,13 +17,14 @@ export default function (file) {
 
         format = format.toLowerCase();
 
-        if (!imsConfig.contentType && !imsConfig.contentType[format]) {
+        //debug('checkFormat', imsConfig);
+        if (!imsConfig.supportedFormats && !imsConfig.supportedFormats[format]) {
           reject('Format not supported...');
           winston.log('warn', `There no option in configuration file for ${imsConfig.contentType}`);
         }
 
-        if (imsConfig.contentType[format]) {
-          file.contentType = imsConfig.contentType[format];
+        if (imsConfig.supportedFormats[format]) {
+          file.contentType = imsConfig.supportedFormats[format];
           winston.log('info', `Supported format, contentType set to: ${file.contentType}`);
           resolve();
         } else {

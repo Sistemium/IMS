0bc94cbf9cc1b093fce81157a258e8b570b09198
Albert <kovalevskij.albert@gmail.com>
2016-02-01 11:36:21 +0200

    generate checksum

--- server/services/generateChecksum.js
+++ server/services/generateChecksum.js
@@ -3,10 +3,12 @@
 const crypto = require('crypto');
 const fs = require('fs');
 const winston = require('winston');
+const debug = require('debug')('ims:generateChecksum');
 
-export default function (image) {
+export default function (pathToFile) {
+  debug('generateChecksum', pathToFile);
   return new Promise(function (resolve, reject) {
-    let stream = fs.createReadStream(image)
+    let stream = fs.createReadStream(pathToFile)
       , hash = crypto.createHash('md5');
 
     stream.on('data', function (data) {
@@ -20,7 +22,7 @@ export default function (image) {
     });
 
     stream.on('error', function (err) {
-      winston.log('error', `Error occurred during reading image ${image}. Error message ${err}`);
+      winston.log('error', `Error occurred during reading image ${pathToFile}. Error message ${err}`);
       reject(err);
     })
   })

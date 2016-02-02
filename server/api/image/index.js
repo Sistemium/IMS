01e95af0e91d63b1d443879e22946d1ffb641d66
Albert <kovalevskij.albert@gmail.com>
2016-02-02 11:05:59 +0200

    create folder before muster

--- server/api/image/index.js
+++ server/api/image/index.js
@@ -4,11 +4,27 @@ const express = require('express');
 const controller = require('./image.controller');
 const multer = require('multer');
 const uploadPath = require('../../config/environment').ims.uploadFolderPath;
-const upload = multer({dest: uploadPath});
+const path = require('path');
+const uuid = require('node-uuid');
+const mkdirp = require('mkdirp');
+
+const destPath = path.normalize(path.join(__dirname, uploadPath, uuid.v4()));
+function setUploadPath() {
+  return function (req, res, next) {
+    mkdirp(destPath, (err) => {
+      if (err) next(err);
+
+      res.locals.folder = destPath;
+      next();
+    });
+  }
+}
+
+const upload = multer({dest: destPath});
 
 var router = express.Router();
 
 router.get('/', controller.get);
-router.post('/', upload.single('file'), controller.post);
+router.post('/', setUploadPath(), upload.single('file'), controller.post);
 
 module.exports = router;
